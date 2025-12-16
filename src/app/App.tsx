4e0530a8443cb7c7a5e0e6629774b06d51f88800
andreygaponik <andreygaponik@gmail.com>
2025-12-16 22:37:59 +0300

    test console

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "@/app/router";
 
 function App() {
+  console.log('some console')
   return (
     <>
       <RouterProvider router={router} />
